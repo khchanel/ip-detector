fd9896851e9dd76e58f3310422f2b79a0775caa2
Nelson Chan <khchanel@gmail.com>
2013-08-26 18:01:39 +1000

    Initial code base

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * This node js web app echo the IP address of the client
+ * Author: Nelson Chan <khchanel@gmail.com>
+ * Date: 26 Aug 2013
+ */
+
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+/* Uses */
+app.use(express.logger());
+
+/* Routes */
+app.get('/', function(req, res) {
+  var clientIp = req.connection.remoteAddress;
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(clientIp);
+});
+
+
+/* bind port to listen on */
+app.listen(PORT);
+console.log("Listening on port: " + PORT);
+
